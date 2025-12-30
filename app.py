42c99dbac3f83de73a1a30d35c783f48fafa9e7f
Shoaib Sheriff <ShoaibSheriff@users.noreply.huggingface.co>
2025-12-30 01:34:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def scribe_audio(audio_path):
     prompt_text = "Transcripción de una charla argentina con lunfardo y modismos de Buenos Aires."
     forced_prompt_ids = scribe_pipe.tokenizer.get_prompt_ids(prompt_text, return_tensors="pt").to("cuda")
 
-    result = scribe_pipe(audio_path, return_timestamps="True", chunk_length_s=30, batch_size=16, generate_kwargs={"language":"spanish", "prompt_ids": forced_prompt_ids})
+    result = scribe_pipe(audio_path, return_timestamps="True", chunk_length_s=30, batch_size=16, generate_kwargs={"condition_on_previous_text":False, "language":"spanish", "prompt_ids": forced_prompt_ids})
 
     return align_speakers(result, diarization_op)
     
